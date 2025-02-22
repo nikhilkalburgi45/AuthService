79656c59bb6b6063928d7a900bd8d1a03eedf11f
nikhilkalburgi45 <nikhilkalburgi21@gmail.com>
2025-02-22 19:30:21 +0530

    Added user signup route to handle user registration via UserController.create

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/ServerConfig");
+const apiRoutes = require("./routes/index");
 
 const setUpAndStartServer = () => {
   const app = express();
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use("/api", apiRoutes);
 
   app.listen(PORT, () => {
     console.log(`Server is up and runnning on ${PORT}`);
