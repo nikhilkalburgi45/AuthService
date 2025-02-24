b2be826f546318706d37d773320212c77538da4c
nikhilkalburgi45 <nikhilkalburgi21@gmail.com>
2025-02-24 21:54:23 +0530

    Added jwt Token

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,6 @@
+const jwt = require("jsonwebtoken");
 const UserRepository = require("../repository/user-repository");
+const { JWT_KEY } = require("../config/ServerConfig ");
 
 class UserService {
   constructor() {
@@ -12,6 +14,24 @@ class UserService {
     } catch (error) {
       console.log("Something went wrong on repo layer");
       throw error;
+    }
+  }
+
+  createToken(user) {
+    try {
+      const result = jwt.sign(user, JWT_KEY, { expiresIn: "1h" });
+      return result;
+    } catch (error) {
+      console.log("Something went wrong while creating token");
+    }
+  }
+
+  verifyToken(user) {
+    try {
+      const response = jwt.verify(token, JWT_KEY);
+      return response;
+    } catch (error) {
+      console.log("Something went wrong while token valideation", error);
     }
   }
 }
